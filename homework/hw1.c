aaa22f01238ed7b5618a5cc9cd6c6c603380624a
wangicheng <r2005.wang@gmail.com>
2023-10-08 11:29:32 +0800

    Update hw1.c
    Add comments and improve program readability

--- homework/hw1.c
+++ homework/hw1.c
@@ -2,12 +2,15 @@
 
 int main(){
     for(int i = 0; i < 81; i++){
+        // 計算當前的 x 與 y
         int x = i / 27 * 3 + (i % 3) + 1;
         int y = (i % 27) / 3 + 1;
-        printf("%d x %d = %2d", x, y, x * y);
+        // 印出等式
+        printf("%d x %d = %2d  ", x, y, x * y);
+        // 每印 3 次要換一行
         if(i % 3 == 2) printf("\n");
-        else printf("  ");
-        if(x % 3 == 0 && y == 9) printf("\n");
+        // 每印 27 次要多換一行
+        if(i % 27 == 26) printf("\n");
     }
     return 0;
 }
